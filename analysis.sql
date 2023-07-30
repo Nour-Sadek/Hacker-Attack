56eb28c184438f523cfd270221ced00c08819833
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-07-30 11:38:00 +0300

    Finished Stage 2's requirements

--- analysis.sql
+++ analysis.sql
@@ -27,3 +27,45 @@ SELECT person_id,
 FROM person
 ORDER BY person_id ASC
 LIMIT 5;
+
+/*
+Stage 2: Who are you?
+
+Your friend created another file. Now you know who the teachers are. 
+First, create the teacher table from teacher.csv. 
+
+Then compare the teacher table with the person table 
+and find out who the students are.
+*/
+
+/* Create the table that will store the data 
+from the teacher.csv file */
+
+CREATE TABLE "teacher" (
+    "person_id" VARCHAR(9) PRIMARY KEY,
+    "class_code" TEXT
+);
+
+-- Import data from teacher.csv file
+.mode csv
+.import --skip 1 teacher.csv teacher
+.mode column 
+
+-- Query to select students from person table
+CREATE VIEW stage_2_query AS
+SELECT person_id, full_name
+FROM person
+WHERE person_id NOT IN (
+    SELECT person_id
+    FROM teacher
+)
+ORDER BY full_name ASC;
+
+SELECT * from stage_2_query
+LIMIT 5;
+
+.print
+
+-- Query to count number of students
+SELECT COUNT(person_id)
+FROM stage_2_query;
