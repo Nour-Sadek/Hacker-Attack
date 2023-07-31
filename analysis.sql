9271d54e2fe6e027d3da2e35e9a073e5deef8469
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-07-31 09:46:31 +0300

    Finished Stage 3 and 4's requirements

--- analysis.sql
+++ analysis.sql
@@ -78,3 +78,71 @@ LIMIT 5;
 -- Query to count number of students
 SELECT COUNT(person_id)
 FROM stage_2_query;
+
+/*
+Stage 3: You are a student
+
+In the last stage, the students were listed. 
+Now create the student table and fill it with data.
+*/
+
+-- Create student table
+
+CREATE TABLE "student" (
+    "person_id" VARCHAR(9) PRIMARY KEY,
+    "grade_code" TEXT
+);
+
+-- Insert the students' person_id data to student table
+INSERT INTO student (person_id)
+SELECT person_id
+FROM stage_2_query;
+
+/* Select all columns from student table. 
+Order the results by person_id ascending. 
+Limit results to '5' */
+SELECT * FROM student
+ORDER BY person_id ASC
+LIMIT 5;
+
+/*
+Stage 4: Plenty of files
+
+Your friends created three more csv files score1.csv, score2.csv, anf score3.csv. 
+These files contain the person_id of the students and the annual score. 
+But, unfortunately, your friends didn't tell you the grade_code of the results. 
+You need to find it out in the following steps.
+
+Now, for this step, first create three temporary tables with the csv files. 
+And then, with the union command, merge them all together.
+*/
+
+-- Create the three tables
+CREATE TABLE "score1" (
+    "person_id" VARCHAR(9),
+    "score" INTEGER
+);
+
+CREATE TABLE "score2" (
+    "person_id" VARCHAR(9),
+    "score" INTEGER
+);
+
+CREATE TABLE "score3" (
+    "person_id" VARCHAR(9),
+    "score" INTEGER
+);
+
+-- Import the data from the three csv files
+.mode csv
+.import --skip 1 score1.csv score1
+.import --skip 1 score2.csv score2
+.import --skip 1 score3.csv score3
+.mode column
+
+-- Merge data from the three score tables
+SELECT * FROM score1
+UNION ALL
+SELECT * FROM score2
+UNION ALL
+SELECT * FROM score3;
